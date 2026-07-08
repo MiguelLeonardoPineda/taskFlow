09e6480e9f7f616e5f8228cbd71e09996473f195
Miguel Pineda <santiagokymm@outlook.com>
2026-07-08 19:00:09 -0400

    Paso 2: refactor ES6+ (filter, find, arrow functions, destructuring, template literals)

--- js/app.js
+++ js/app.js
@@ -1,23 +1,18 @@
-// Ahora importamos el GESTOR, no la Tarea directamente:
-// será el gestor quien cree las tareas por nosotros.
 import { GestorTareas } from "./clases/GestorTareas.js";
 
-// Creamos UN archivador para toda la aplicación.
 const gestor = new GestorTareas();
 
-// Agregamos tres tareas.
 gestor.agregarTarea("Estudiar programación orientada a objetos");
 gestor.agregarTarea("Terminar el proyecto del módulo 4");
 gestor.agregarTarea("Hacer commit en Git");
 
 console.log("Tareas después de agregar:", gestor.tareas);
 
-// Cambiamos el estado de la segunda tarea (índice 1).
-const idSegunda = gestor.tareas[1].id;
-gestor.cambiarEstadoTarea(idSegunda);
-console.log("Estado de la segunda tarea:", gestor.tareas[1].estado);
+// Cambiamos el estado de la segunda tarea usando destructuring.
+const [primera, segunda] = gestor.tareas;
+gestor.cambiarEstadoTarea(segunda.id);
+console.log(`Estado de la segunda tarea: ${segunda.estado}`);
 
-// Eliminamos la primera tarea (índice 0) usando su id.
-const idPrimera = gestor.tareas[0].id;
-gestor.eliminarTarea(idPrimera);
+// Eliminamos la primera tarea.
+gestor.eliminarTarea(primera.id);
 console.log("Tareas después de eliminar la primera:", gestor.tareas);
--- js/clases/GestorTareas.js
+++ js/clases/GestorTareas.js
@@ -1,40 +1,28 @@
-// Importamos la clase Tarea porque el gestor necesita CREAR tareas.
-// Están en la misma carpeta, por eso la ruta empieza con "./"
 import { Tarea } from "./Tarea.js";
 
 export class GestorTareas {
 
   constructor() {
-    this.tareas = []; // La lista arranca vacía: aún no hay ninguna tarea.
+    this.tareas = [];
   }
 
-  // Crea una nueva tarea a partir de una descripción y la mete en la lista.
+  // Crea una nueva tarea y la agrega a la lista.
   agregarTarea(descripcion) {
     const nuevaTarea = new Tarea(descripcion);
     this.tareas.push(nuevaTarea);
     return nuevaTarea;
   }
 
-  // Elimina de la lista la tarea cuyo id coincida con el recibido.
+  // Elimina una tarea: nos quedamos con TODAS menos la del id recibido.
   eliminarTarea(id) {
-    for (let i = 0; i < this.tareas.length; i++) {
-      if (this.tareas[i].id === id) {
-        this.tareas.splice(i, 1);
-        break;
-      }
-    }
+    this.tareas = this.tareas.filter((tarea) => tarea.id !== id);
   }
 
-  // Busca una tarea por su id y le pide que cambie su propio estado.
+  // Busca la tarea por id y le pide que cambie su propio estado.
   cambiarEstadoTarea(id) {
-  console.log("Buscando id:", id, "tipo:", typeof id);
-  for (let i = 0; i < this.tareas.length; i++) {
-    console.log("Comparando con:", this.tareas[i].id, "tipo:", typeof this.tareas[i].id);
-    if (this.tareas[i].id === id) {
-      console.log("¡Coincidencia encontrada!");
-      this.tareas[i].cambiarEstado();
-      break;
+    const tareaEncontrada = this.tareas.find((tarea) => tarea.id === id);
+    if (tareaEncontrada) {
+      tareaEncontrada.cambiarEstado();
     }
   }
-}
 }
